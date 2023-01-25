181767cecc8eb4c6cdca348d1e824338721651bf
preetiag18 <preetiag18@gmail.com>
2023-01-25 13:57:09 +0200

    validation functionality

--- projectmanager-typescript/src/app.ts
+++ projectmanager-typescript/src/app.ts
@@ -1,5 +1,50 @@
-// console.log("PMApp in TS begins...");
+// interfaces------
+interface Validatable {
+  value: string | number;
+  requied?: boolean;
+  minLength: number;
+  maxLength: number;
+  min?: number;
+  max?: number;
+}
 
+function validate(validatableInput: Validatable) {
+  let isValid = true;
+  if (validatableInput.requied) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+  }
+  if (
+    validatableInput.minLength != null &&
+    typeof validatableInput.value === "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length >= validatableInput.minLength;
+  }
+
+  if (
+    validatableInput.maxLength != null &&
+    typeof validatableInput.value === "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length <= validatableInput.maxLength;
+  }
+
+  if (
+    validatableInput.min != null &&
+    typeof validatableInput.value === "number"
+  ) {
+    isValid = isValid && validatableInput.value >= validatableInput.min;
+  }
+
+  if (
+    validatableInput.max != null &&
+    typeof validatableInput.value === "number"
+  ) {
+    isValid = isValid && validatableInput.value <= validatableInput.max;
+  }
+}
+
+// decorator is a function
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value;
   const adjDescriptor: PropertyDescriptor = {
