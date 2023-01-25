10a2315323f819b74828737d4ec4a68dc7bdb2b1
preetiag18 <preetiag18@gmail.com>
2023-01-25 13:42:53 +0200

    fetching user input

--- projectmanager-typescript/src/app.ts
+++ projectmanager-typescript/src/app.ts
@@ -62,9 +62,39 @@ class ProjectInput {
     this.attach();
     this.configure();
   }
+
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredPeople.trim().length === 0
+    ) {
+      alert("Invalid input,please try again");
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
+  }
+
   @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
+    const userInput = this.gatherUserInput();
+    // console.log(userInput);
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+      this.clearInputs();
+    }
     console.log(this.titleInputElement.value);
   }
   private attach() {
